22346fdd4c7be933c3de42a773fb83b93f4fac6a
Daniel-Andarge <andargedaniel90@gmail.com>
2024-04-30 01:50:47 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project focuses on the detailed analysis of a large corpus of financial news data to discover correlations between news sentiment and stock market movements.',
+    author='Daniel Andarge',
+    license='',
+)
