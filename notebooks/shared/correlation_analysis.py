7e27ae2aafeccd12dbe93c47f177a1b85797ae05
Daniel-Andarge <andargedaniel90@gmail.com>
2024-05-05 20:03:07 +0300

    Tests

--- notebooks/shared/correlation_analysis.py
+++ notebooks/shared/correlation_analysis.py
@@ -1,19 +1,24 @@
 import pandas as pd
 
 def align_datasets(sentiment_data, stock_price_data):
-
     sentiment_data['date'] = pd.to_datetime(sentiment_data['date'])
     stock_price_data['Date'] = pd.to_datetime(stock_price_data['Date'])
 
     sentiment_data.set_index('date', inplace=True)
     stock_price_data.set_index('Date', inplace=True)
 
+    # Convert timezone-naive indexes to timezone-aware indexes
+    
+    stock_price_data.index = stock_price_data.index.tz_localize('UTC')
+
     # Align datasets based on the date index
     aligned_data = sentiment_data.join(stock_price_data, how='inner')
 
-    
-    start_date = max(sentiment_data.index.min(), stock_price_data.index.min())
-    end_date = min(sentiment_data.index.max(), stock_price_data.index.max())
+    # Determine start and end dates for the aligned data
+    start_date = max(aligned_data.index.min(), stock_price_data.index.min())
+    end_date = min(aligned_data.index.max(), stock_price_data.index.max())
+
+    # Filter the aligned data based on start and end dates
     aligned_data = aligned_data.loc[start_date:end_date]
 
     return aligned_data
